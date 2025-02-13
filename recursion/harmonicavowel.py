b2f75875a8808bef766d98bce2360a6f5c0401eb
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-02-13 15:04:53 +0530

    done with life man

--- recursion/harmonicavowel.py
+++ recursion/harmonicavowel.py
@@ -1,6 +1,15 @@
 def lenofharmony(s):
+    count = 0
+    c = "bcdfghjklmnpqrstvwxy"
+    for ch in s:
+        if ch in c:
+            count += 1
+        elif ch in "0123456789":
+            count += 1
     if len(s) < 5:
         return 0
+    elif count > 0:
+        return 0 
     else:
         i = 0
         j = 0
